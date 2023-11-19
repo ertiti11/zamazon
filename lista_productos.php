6a9cfe0ecb15804d7fb04c2e2dc6db206c6932b1
ertiti11 <vatitiprieto11@gmail.com>
2023-11-20 00:54:00 +0100

    POC de nueva tabla de lista de productos

--- lista_productos.php
+++ lista_productos.php
@@ -109,37 +109,58 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <?php if (isset($success_msg)) echo "<div class='alert alert-success' style='width:33em' role='alert'>" . $success_msg . "</div>" ?>
     </div>
     <?php if (isset($error)) echo "<div class='alert alert-danger' role='alert'>" . $error . "</div>" ?>
-    <div style="display: flex; justify-content:center; align-items:center">
+    <section style="display: flex; flex-direction: column; padding: 1rem;">
 
-        <table class='table table-borderless w-50'>
-            <thead class='table-primary'>
+        <table style="width: 100%; text-align: left; border-collapse: collapse;">
+            <thead style="border-bottom: 1px solid;">
                 <tr>
-                    <th scope='col'>imagen</th>
-                    <th scope='col'>nombre</th>
-                    <th scope='col'>precio</th>
-                    <th scope='col'>descripcion</th>
-                    <th scope='col'>cantidad</th>
-                    <th scope='col'></th>
+                    <th style="padding: 0.5rem;">Product</th>
+                    <th style="padding: 0.5rem;">Image</th>
+                    <th style="padding: 0.5rem;">Name</th>
+                    <th style="padding: 0.5rem;">Description</th>
+                    <th style="padding: 0.5rem;">Price</th>
+                    <th style="padding: 0.5rem;">Quantity</th>
+                    <th style="padding: 0.5rem;">Action</th>
                 </tr>
             </thead>
+
             <tbody>
                 <?php
                 foreach ($productos as $producto) {
-                    echo '<tr>';
-                    echo '<td><img src="' . $producto->imagen . '" alt="' . $producto->nombreProducto . '" width="100" height="100"></td>';
-                    echo '<td>' . $producto->nombreProducto . '</td>';
-                    echo '<td>' . $producto->precio . '€ </td>';
-                    echo '<td>' . $producto->descripcion . '</td>';
-                    echo '<td>' . $producto->cantidad . '</td>';
-                    echo "<td>";
-                    echo '<form method="post">';
+                    echo '<tr style="border-bottom: 2px solid #DADADA;">';
+                    echo '<td style="padding: 0.5rem;"><img src="' . $producto->imagen . '" alt="' . $producto->nombreProducto . '" width="100" height="100"></td>';
+                    echo '<td style="padding: 0.5rem;">' . $producto->nombreProducto . '</td>';
+                    echo '<td style="padding: 0.5rem;">' . $producto->precio . '€ </td>';
+                    echo '<td style="padding: 0.5rem;">' . $producto->descripcion . '</td>';
+                    echo '<td style="padding: 0.5rem;">' . $producto->cantidad . '</td>';
+                    echo '<td style="padding: 0.5rem;">';
+                    echo '<form method="post" class="d-flex gap-4">';
                     echo "<input type='hidden' name='nombre' value='$producto->id'></input>";
-                    echo '<select name="cantidad" id="cantidad">';
+                    echo '<select name="cantidad" style="
+                    display: flex;
+                    height: 2.5rem;
+                    width: 100%;
+                    align-items: center;
+                    justify-content: space-between;
+                    border-radius: 0.375rem;
+                    border: 1px solid #e2e8f0;
+                    background-color: #f7fafc;
+                    padding-left: 0.75rem;
+                    padding-right: 0.75rem;
+                    padding-top: 0.5rem;
+                    padding-bottom: 0.5rem;
+                    font-size: 0.875rem;
+                    outline: none;
+                    border-color: transparent;
+                    box-shadow: 0 0 0 2px #ebf4ff;
+                    placeholder: #a0aec0;
+                    opacity: 0.5;
+                  " id="cantidad">';
                     for ($i = 1; $i <= 5; $i++) {
                         echo '<option value="' . $i . '">' . $i . '</option>';
                     }
                     echo '</select>';
-                    echo "<input type='submit' class='btn btn-primary' onclick='mostrarToast()''  value='añadir a la cesta'></input>";
+                    echo "<input type='submit' class='p-2' style='background-color:black;color:white;border-radius:8px;border:none' value='añadir a la cesta'></input>";
                     echo "</form>";
                     echo "</td>";
                     echo '</tr>';
@@ -148,7 +169,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo "</table><br>";
 
                 ?>
-    </div>
+                </div>
 
 </body>
 
